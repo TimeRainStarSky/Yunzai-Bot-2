809b8c483416c840ed5833b61e6fd37aa693063a
Sirly <45280500+SirlyDreamer@users.noreply.github.com>
2022-06-25 18:15:18 +0800

    添加喵喵插件的自动更新，增加自动暂存更改

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,52 @@
 #!/bin/sh
 set -e
 
-echo -e "\n ======== \n ${Info} ${GreenBG} 拉取最新项目 ${Font} \n ======== \n"
-git pull
+GreenBG="\\033[42;37m"
+YellowBG="\\033[43;37m"
+BlueBG="\\033[44;37m"
+Font="\\033[0m"
 
-echo -e "\n ======== \n ${Info} ${GreenBG} 当前版本信息 ${Font} \n ======== \n"
+Version="${BlueBG}[版本]${Font}"
+Info="${GreenBG}[信息]${Font}"
+Warn="${YellowBG}[提示]${Font}"
+
+WORK_DIR="/app/Yunzai-Bot"
+MIAO_PLUGIN_PATH="/app/Yunzai-Bot/plugins/miao-plugin"
+
+echo -e "\n ================ \n ${Info} ${GreenBG} 拉取 Yunzai-Bot 更新 ${Font} \n ================ \n"
+if [[ -z $(git status -s) ]]; then
+    echo -e " ${Warn} ${YellowBG} 当前工作区有修改，尝试暂存后更新。${Font}"
+    git add .
+    git stash
+    git pull origin master --allow-unrelated-histories
+    git stash pop
+else
+    git pull origin master --allow-unrelated-histories
+fi
+
+echo -e "\n ================ \n ${Version} ${BlueBG} Yunzai-Bot 版本信息 ${Font} \n ================ \n"
 git log -1 --pretty=format:"%h - %an, %ar (%cd) : %s"
 
-echo -e "\n ======== \n ${Info} ${GreenBG} 更新运行依赖 ${Font} \n ======== \n"
+echo -e "\n ================ \n ${Info} ${GreenBG} 更新 Yunzai-Bot 运行依赖 ${Font} \n ================ \n"
 npm install
 
-echo -e "\n ======== \n ${Info} ${GreenBG} 启动云崽-BOT ${Font} \n ======== \n"
+if [ -d $MIAO_PLUGIN_PATH ]; then
+    echo -e "\n ================ \n ${Info} ${GreenBG} 拉取喵喵插件更新 ${Font} \n ================ \n"
+    cd $MIAO_PLUGIN_PATH
+    if [[ ! -z $(git status -s) ]]; then
+        echo -e " ${Warn} ${YellowBG} 当前工作区有修改，尝试暂存后更新。${Font}"
+        git add .
+        git stash
+        git pull origin master --allow-unrelated-histories
+        git stash pop
+    else
+        git pull origin master --allow-unrelated-histories
+    fi
+    echo -e "\n ================ \n ${Version} ${BlueBG} 喵喵插件版本信息 ${Font} \n ================ \n"
+    git log -1 --pretty=format:"%h - %an, %ar (%cd) : %s"
+fi
+
+cd $WORK_DIR
+
+echo -e "\n ======== \n ${Info} ${GreenBG} 启动 Yunzai-Bot ${Font} \n ======== \n"
 node app
