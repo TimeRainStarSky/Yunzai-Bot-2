18aa882ffb4dd503ff8a2a45cc09d3848f9c65ac
yoimiya-kokomi <592981798@qq.com>
2022-06-29 05:29:31 +0800

    增加体力推送、新闻推送的config配置开关

--- config/config_default.js
+++ config/config_default.js
@@ -6,12 +6,12 @@
 let config = {
   //qq账号 密码
   account: {
-    qq:  "",  //账号
+    qq: "",  //账号
     pwd: "",  //密码，可为空则用扫码登录
-    log_level:"info",//日志等级:trace,debug,info,warn,error,fatal,mark,off
-    platform:5,      //1:安卓手机、 2:aPad 、 3:安卓手表、 4:MacOS 、 5:iPad
-    autoFriend:1,    //1-自动同意加好友 0-好友请求不处理
-    autoQuit:1,      //1-自动退小群 0-不处理
+    log_level: "info",//日志等级:trace,debug,info,warn,error,fatal,mark,off
+    platform: 5,      //1:安卓手机、 2:aPad 、 3:安卓手表、 4:MacOS 、 5:iPad
+    autoFriend: 1,    //1-自动同意加好友 0-好友请求不处理
+    autoQuit: 1,      //1-自动退小群 0-不处理
   },
 
   //redis配置(默认配置就好，一般都不用改，使用docker搭建的用户需要更改host项)
@@ -46,13 +46,15 @@ let config = {
   //默认cookie帮助文档链接地址，其他ck帮助链接
   //www.wolai.com/tremorstar/jRjczxsfdsmzWDaDVVwVEM
   //yakultoo.feishu.cn/docs/doccnnepmeWeweGSbjgBM8vIsCe
-  cookieDoc:"docs.qq.com/doc/DUWNVQVFTU3liTVlO",
+  cookieDoc: "docs.qq.com/doc/DUWNVQVFTU3liTVlO",
 
   //推送任务
-  pushTask:{
+  pushTask: {
     signTime: "0 2 0 * * ?", //签到任务执行时间，Cron表达式，默认00:02开始执行，每10s签到一个
     isPushSign: 1,           //是否推送签到成功消息 1-推送 0-关闭
     isPushLedger: 1,         //是否推送每月原石统计 1-推送 0-关闭
+    isPushNote: 1,           //是否推送体力 1-推送，0-关闭
+    isPushNews: 1,           //是否推送新闻 1-推送，0-关闭
   },
 
   //群设置
@@ -61,20 +63,20 @@ let config = {
     'default': {
       delMsg: 0,        //隔多少毫秒后撤回消息（十连），0不撤回
       gachaDayNum: 1,   //每天抽卡次数，限制次数，防止刷屏,4点重置
-      LimitSeparate:0,  //角色池，武器池限制分开计算 0-不分开 1-分开
+      LimitSeparate: 0,  //角色池，武器池限制分开计算 0-不分开 1-分开
 
       //米游社信息查询
       mysDayLimit: 30,  //每天每人查询次数
       mysUidLimit: 5,   //每天每人查询uid个数
 
-      groupCD:  500,    //群聊中所有指令操作冷却时间，单位毫秒,0则无限制
+      groupCD: 500,    //群聊中所有指令操作冷却时间，单位毫秒,0则无限制
       singleCD: 2000,   //群聊中个人操作冷却时间，单位毫秒
-      PokeCD:   10000,  //群聊中戳一戳冷却时间，单位毫秒
+      PokeCD: 10000,  //群聊中戳一戳冷却时间，单位毫秒
 
-      imgAddLimit:0,     //添加表情是否限制  0-所有群员都可以添加 1-群管理员才能添加 2-主人才能添加
-      imgMaxSize:2,      //添加表情图片大小限制，默认1m
+      imgAddLimit: 0,     //添加表情是否限制  0-所有群员都可以添加 1-群管理员才能添加 2-主人才能添加
+      imgMaxSize: 2,      //添加表情图片大小限制，默认1m
 
-      imgRate:5,         //随机表情回复概率0-100 需要配置百度ocr 配置往下滚
+      imgRate: 5,         //随机表情回复概率0-100 需要配置百度ocr 配置往下滚
 
       onlyReplyAt: false, // 只关注主动at bot的信息
       botAlias: "云崽",  // 在此群bot的名字，如果以此前缀开头，则也认为是at
@@ -87,53 +89,53 @@ let config = {
 
     //每个群的单独配置(用于覆盖通用配置)
     //配置项和默认default一样，自行选择
-    '213938015':{
+    '213938015': {
       delMsg: 0,        //隔多少毫秒后撤回消息（十连），0不撤回
       gachaDayNum: 1,   //每天抽卡次数，限制次数，防止刷屏,4点重置
-      LimitSeparate:0,  //角色池，武器池限制分开计算 0-不分开 1-分开
+      LimitSeparate: 0,  //角色池，武器池限制分开计算 0-不分开 1-分开
 
       //米游社信息查询
       mysDayLimit: 30,  //每天每人查询次数
       mysUidLimit: 5,   //每天每人查询uid个数
 
-      groupCD:  500,    //群聊中所有指令操作冷却时间，单位毫秒,0则无限制
+      groupCD: 500,    //群聊中所有指令操作冷却时间，单位毫秒,0则无限制
       singleCD: 2000,   //群聊中个人操作冷却时间，单位毫秒
-      PokeCD:   10000,  //群聊中戳一戳冷却时间，单位毫秒
+      PokeCD: 10000,  //群聊中戳一戳冷却时间，单位毫秒
       disable: ['all'],//禁用所有功能
     },
-    'qq群号':{
+    'qq群号': {
       disable: ['all'],//禁用所有功能
     }
   },
 
   //主人qq，米游社查询，十连不受限制
-  masterQQ:[123456,],
+  masterQQ: [123456,],
 
   //黑名单qq
-  balckQQ:[
+  balckQQ: [
     2854196310,
   ],
   //黑名单q群
-  balckGroup:[
-    213938015,123456,
+  balckGroup: [
+    213938015, 123456,
   ],
 
   //对话ai,配置后@机器人进行聊天对话，推荐配置将你的机器人变成人工智障
   //腾讯智能对话平台 https://cloud.tencent.com/product/tbp 
   //目前免费使用的，自行认证申请
-  tencentAI:{
-    secretId:  "",//云产品-管理与审计-访问秘钥获取
+  tencentAI: {
+    secretId: "",//云产品-管理与审计-访问秘钥获取
     secretKey: "",//云产品-管理与审计-访问秘钥获取
-    BotId:     "",//腾讯智能对话平台-Bot信息-BotId
-    BotEnv:    "dev",//不用管就填dev
-    BotName:   "云崽",//机器人名称
+    BotId: "",//腾讯智能对话平台-Bot信息-BotId
+    BotEnv: "dev",//不用管就填dev
+    BotName: "云崽",//机器人名称
   },
 
   //百度ocr https://ai.baidu.com/ai-doc/OCR/dk3iqnq51
   //智障斗图用
-  BaiduOcr:{
-    APP_ID:     "",
-    API_KEY:    "",
+  BaiduOcr: {
+    APP_ID: "",
+    API_KEY: "",
     SECRET_KEY: "",
   },
 };
